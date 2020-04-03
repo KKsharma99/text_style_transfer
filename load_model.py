100e21558977f7cceb67cb714775c767e0b43550
Sarah Chen <schen1337@gmail.com>
2020-04-03 19:28:45 -0400

    changed load_state_dict

--- load_model.py
+++ load_model.py
@@ -6,7 +6,26 @@ from src.vocabulary import Vocabulary
 from src.style_transfer import StyleTransfer
 from scripts.train_model import loadParams
 
-model = torch.load('/Users/schen1337/Documents/text_style_transfer/data/models/yelp/experiment_0_models/model-2020-04-01-epoch_9-loss_16.367482.pt')
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_file_style1", type=str)
+parser.add_argument("--train_file_style2", type=str)
+parser.add_argument("--evaluation_file_style1", type=str)
+parser.add_argument("--evaluation_file_style2", type=str)
+parser.add_argument("--vocabulary", type=str)
+parser.add_argument("--savefile", type=str)
+parser.add_argument("--logdir", type=str, default="")
+args = parser.parse_args()
+
+params = loadParams()
+params.savefile = args.savefile
+params.logdir = args.logdir
+vocab = Vocabulary()
+vocab.loadVocabulary(args.vocabulary)
+vocab.initializeEmbeddings(params.embedding_size)
+
+model = StyleTransfer(params, vocab)
+model.load_state_dict(torch.load('/Users/schen1337/Documents/text_style_transfer/data/models/yelp/experiment_0_models/model-2020-04-01-epoch_9-loss_16.367482.pt'))
+# model = torch.load('/Users/schen1337/Documents/text_style_transfer/data/models/yelp/experiment_0_models/model-2020-04-01-epoch_9-loss_16.367482.pt')
 model.eval()
 
 sents = []
